c790fa2d4ce2356a0c7835a878badcf7539411d1
JoelTorcato <a12022@ae4outubro.pt>
2025-10-03 12:41:18 +0100

    Update SQL exercise data and queries
    Replaced sample data in Clube, Atleta, and Clube_Atleta tables with new entries. Updated all related queries to reflect the new data, added several new query examples, and improved formatting for readability.

--- 2025/September/26 Sep/Exercício um/Exercício um.sql
+++ 2025/September/26 Sep/Exercício um/Exercício um.sql
@@ -26,37 +26,46 @@ create table Clube_Atleta(
 );
 
 insert into Clube values
-  ('Águias','Rua das Águias','Futebol',20),
-  ('Bracarense','Av. Braga','Futebol',15);
+  ('Alverca','Rua dos Moinhos','Basquetebol',3),
+  ('Bucelas','Av. Brasil','Surf',11);
 
 insert into Atleta values
-  ('João Silva','BI123',22,10000,'912345678'),
-  ('Ana Costa','',20,15000,NULL);
+  ('Joel Fernandes','1Q2W3E45',16,10000,'938112685'),
+  ('Simão Torcato','',20,15000,NULL);
 
 insert into Clube_Atleta values
-  ('Águias','João Silva'),
-  ('Bracarense','Ana Costa');
+  ('Alverca','Joel Fernandes'),
+  ('Bucelas','Simão Torcato');
 
 -- a
-select * from Atleta where Idade_Atleta<25;
+select * from Atleta
+where Idade_Atleta<25;
 
 -- b
-select Nome_Atleta, Nome_Clube from Clube_Atleta;
+select Nome_Atleta, Nome_Clube
+from Clube_Atleta;
 
 -- c
-select count(*) as Total_Atletas from Clube_Atleta where Nome_Clube='Águias';
+select count(*)
+as Total_Atletas
+from Clube_Atleta
+where Nome_Clube='Alverca';
 
 -- d
-select avg(Atleta.Valor_Passe) from Atleta;
+select avg(Atleta.Valor_Passe)
+from Atleta;
 
 -- e
 select Nome_Atleta
 from Clube_Atleta
-where Nome_Clube <> 'Bracarense'
+where Nome_Clube <> 'Bucelas'
 and Nome_Atleta in (select Nome_Atleta from Atleta where Idade_Atleta = 20);
 
 -- select (f)
-select Nome_Atleta from Atleta where Telefone is null or Telefone = '';
+select Nome_Atleta
+from Atleta
+where Telefone
+is null or Telefone = '';
 
 -- describe
 describe Clube;
@@ -73,14 +82,14 @@ where Idade_Clube > 10;
 
 -- b
 update Atleta set Valor_Passe = 1000000
-where Nome_Atleta = 'Ana Costa';
+where Nome_Atleta = 'Joel Fernandes';
 select * from Atleta;
 
 -- c
 delete from Atleta
 where Idade_Atleta > 35;
 
--- d 
+-- d
 select distinct Clube_Atleta.Nome_Clube
 from Clube_Atleta
 inner join Atleta on Clube_Atleta.Nome_Atleta = Atleta.Nome_Atleta
@@ -91,4 +100,29 @@ select Nome_Atleta
 from Atleta
 where Nome_Atleta like 'A%';
 
+-- f
+select Nome_Clube
+from Clube_Atleta
+group by Nome_Clube
+having count(Nome_Atleta) > 3;
 
+-- g
+select Nome_Atleta, Valor_Passe
+from Atleta
+order by Valor_Passe desc;
+
+-- h
+select * from Atleta
+where Idade_Atleta
+between 18 and 30;
+
+-- i
+select Nome_Clube
+from Clube
+where Nome_Clube
+not in (select Nome_Clube from Clube_Atleta);
+
+-- j
+select Nome_Atleta, Nome_Clube
+from Clube_Atleta
+order by Nome_Clube;
